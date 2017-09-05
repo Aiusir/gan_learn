16ce2dac0244af22dbd0912948593f40dfaee1ae
Aiusir <625305702@qq.com>
2017-09-05 21:03:27 +0800

    fix bugs,it finally worked!

--- cgan_mlp.py
+++ cgan_mlp.py
@@ -1,17 +1,17 @@
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
-import numpy as np
 import random
+import os
 
 
-class Generator():
+class Generator:
     def __init__(self,batch_size=128,x_size=(28,28),h_dim = 128):
         self.reuse = False
         self.x_size = x_size
         self.batch_size = batch_size
         self.h_dim = h_dim
 
-    def __call__(self,z,y,name =''):
+    def __call__(self,z,y,training=False,name =''):
         inputs = tf.concat([z,y],1)
         inputs = tf.convert_to_tensor(inputs)
         with tf.variable_scope('g',reuse = self.reuse):
@@ -19,37 +19,38 @@ class Generator():
             with tf.variable_scope('reshape'):
                 outputs = tf.reshape(inputs,[self.batch_size,-1])
             with tf.variable_scope('dense1'):
-                outputs = tf.layers.dense(outputs,self.h_dim,activation = tf.nn.relu)
+                outputs = tf.layers.dense(outputs,self.h_dim,activation = tf.nn.relu,trainable = training)
             with tf.variable_scope('dense2'):
-                outputs = tf.layers.dense(outputs,self.x_size[0]*self.x_size[1],activation = tf.nn.sigmoid)
-            self.reuse = True
-            self.variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES,scope = 'g')
+                outputs = tf.layers.dense(outputs,self.x_size[0]*self.x_size[1],activation = tf.nn.sigmoid,trainable = training)
+        self.reuse = True
+        self.variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES,scope = 'g')
         return outputs
 
 
-class Discriminator():
+class Discriminator:
     def __init__(self,batch_size=128,h_dim = 128):
         self.reuse = False
         self.batch_size = batch_size
         self.h_dim = h_dim
 
-    def __call__(self,x,y,name=''):
+    def __call__(self,x,y,training=False,name=''):
         inputs = tf.concat([x,y],1)
         inputs = tf.convert_to_tensor(inputs)
-        with tf.variable_scope('d',reuse = self.reuse):
+        with tf.name_scope('d'+name), tf.variable_scope('d',reuse = self.reuse):
             # reshape from inputs
             with tf.variable_scope('reshape'):
                 outputs = tf.reshape(inputs,[self.batch_size,-1])
             with tf.variable_scope('dense1'):
-                outputs_logits = tf.layers.dense(outputs,self.h_dim,activation = tf.nn.relu)
+                outputs = tf.layers.dense(outputs,self.h_dim,activation = tf.nn.relu,trainable = training)
             with tf.variable_scope('dense2'):
-                outputs_prob = tf.layers.dense(outputs_logits,1,activation = tf.nn.sigmoid)
-            self.reuse = True
-            self.variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES,scope = 'd')
+                outputs_logits = tf.layers.dense(outputs,1,trainable = training)
+                outputs_prob = tf.nn.sigmoid(outputs_logits)
+        self.reuse = True
+        self.variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES,scope = 'd')
         return outputs_prob,outputs_logits
 
 
-class CGANMLP():
+class CGANMLP:
     def __init__(self,batch_size=128,x_size = (28,28),h_dim = 128,z_dim = 100):
         self.batch_size = batch_size
         self.h_dim = h_dim
@@ -60,16 +61,16 @@ class CGANMLP():
         self.z = tf.random_uniform([self.batch_size,self.z_dim],minval = -1.0,maxval = 1.0)
     
     def loss(self,x_in,y_in):
-        '''build model,calculate losses.
+        """build model,calculate losses.
         Args:
             x_in:4-D Tensor of shape '[batch,height,width,channels]'
             y_in:2-D Tensor of shape '[batch,label_num]'
         Returns:
             dict of each models loss.
-        '''
-        generated = self.g(self.z,y_in)
-        d_real,d_logits_real = self.d(x_in,y_in,name = 'd')
-        d_fake,d_logits_fake = self.d(generated,y_in,name = 'g')
+        """
+        generated = self.g(self.z,y_in,training = True)
+        d_real,d_logits_real = self.d(x_in,y_in,training = True,name = 'd')
+        d_fake,d_logits_fake = self.d(generated,y_in,training = True,name = 'g')
 
         d_loss_real = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=d_logits_real,labels = tf.ones_like(d_logits_real)))
         d_loss_fake = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=d_logits_fake,labels = tf.zeros_like(d_logits_fake)))
@@ -78,22 +79,22 @@ class CGANMLP():
         return {self.g:g_loss,self.d:d_loss}
 
     def train(self,losses,learning_rate = 0.0002):
-        '''
+        """
         Args:
             losses dict
         Returns:
             train op.
-        '''
+        """
         d_opt_op = tf.train.AdamOptimizer(learning_rate).minimize(losses[self.d],var_list = self.d.variables)
         g_opt_op = tf.train.AdamOptimizer(learning_rate).minimize(losses[self.g],var_list = self.g.variables)
 
-        with tf.control_dependencies([d_opt_op,g_opt_op]):
+        with tf.control_dependencies([g_opt_op,d_opt_op]):
             return tf.no_op(name = 'train')
 
     def sample_images(self,inputs_z,x_size=(28,28),row=8,col =8,inputs = None):
         if inputs is None:
             inputs = self.z
-        imgs = self.g(inputs,inputs_z)
+        imgs = self.g(inputs,inputs_z,training = True)
         imgs = tf.reshape(imgs,[self.batch_size,x_size[0],x_size[1],1])
         imgs = tf.image.convert_image_dtype(imgs,tf.uint8)
         imgs = [img for img in tf.split(imgs,self.batch_size,axis = 0)]
@@ -104,7 +105,7 @@ class CGANMLP():
         img = tf.concat(rows,1)
         return tf.image.encode_jpeg(tf.squeeze(img,[0]))
 
-def train():
+def mnisttrain():
     batch_size = 128
     mnist = input_data.read_data_sets('MNIST_data',one_hot=True)
 
@@ -113,13 +114,13 @@ def train():
     y_data = tf.placeholder(tf.float32,[batch_size,10],name = 'y_data')
 
     losses = cgan.loss(x_data,y_data)
-    train_op = cgan.train(losses,learning_rate = 0.0001)
+    train_op = cgan.train(losses,learning_rate = 0.001)
 
     with tf.Session() as sess:
         saver = tf.train.Saver()
         sess.run(tf.global_variables_initializer())
 
-        for step in range(1000000):
+        for step in range(500000):
             x_in,y_in = mnist.train.next_batch(batch_size)
             _,g_loss_value,d_loss_value = sess.run([train_op,losses[cgan.g],losses[cgan.d]],feed_dict = {x_data:x_in,y_data:y_in})
 
@@ -137,8 +138,26 @@ def train():
                     f.write(generated)
         saver.save(sess,os.path.join('./model','model.ckpt'))
 
+def mnisttest():
+    batch_size = 128
+    cgan = CGANMLP()
+
+    with tf.Session() as sess:
+        saver = tf.train.Saver()
+        sess.run(tf.global_variables_initializer())
+        saver.restore(sess,os.path.join('./model','model.ckpt'))
+
+        label = random.randint(0,9)
+        y_input = tf.one_hot(label,depth = 10)
+        y_titl = tf.tile(y_input,[batch_size])
+        y_input = tf.reshape(y_titl,[batch_size,10])
+        images = cgan.sample_images(y_input)
+        generated = sess.run(images)
+        with open('train%d.jpg'%label,'wb')as f:
+            f.write(generated)
+
 if __name__ == '__main__':
-    train()
+    mnisttrain()
 
 
 
